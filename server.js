15d3a5837eca92d7a50d082948d8d473fe3e9774
Alex <test@mail.ru>
2022-01-23 18:14:31 +0300

    added3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 //const cors = require('cors');
 const path = require('path');
 //const bodyParser = require('body-parser');
-//const pool = require('./db');
+const pool = require('./db');
 const port = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") {
@@ -17,13 +17,22 @@ if (process.env.NODE_ENV === "production") {
     console.log("api");
     console.log(process.env.NODE_ENV);
     res.send({ message: "Hello world"});
-    //res.status(200).json({ message: "I am here!"});
   });
 
   app.get('/api', (req, res) => {
     console.log("api_new");
     console.log(process.env.NODE_ENV);
     res.send({ message: "Hello world"});
+  });
+
+  app.get('/types', async (req, res) => {
+    console.log("types");
+    try {
+      const types = await pool.query('SELECT * FROM types');
+      res.json(types.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
   });
 
   app.get('*', (req, res) => {
