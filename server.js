4618a1ec8e57dfa1e5ebdd3e8d34f3c002f0ff31
Alex <test@mail.ru>
2022-01-23 17:45:12 +0300

    added3

--- server.js
+++ server.js
@@ -25,6 +25,18 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.get('/', (req, res) => {
+  console.log("api");
+  console.log(process.env.NODE_ENV);
+  res.send({ message: "Hello world"});
+  //res.status(200).json({ message: "I am here!"});
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'));
+});
+
 
 // app.use(express.static(__dirname));
 // app.use(express.static(path.join(__dirname, './build')));
@@ -54,19 +66,19 @@ app.get('/api', (req, res) => {
 //app.use(cors());
 
 
-app.get('/users', async (req, res) => {
-  console.log("users")
-  try {
-      const users = await pool.query('SELECT * FROM users');
-      res.json(users.rows);
-  } catch (err) {
-      console.error(err.message);
-  }
-});
+// app.get('/users', async (req, res) => {
+//   console.log("users")
+//   try {
+//       const users = await pool.query('SELECT * FROM users');
+//       res.json(users.rows);
+//   } catch (err) {
+//       console.error(err.message);
+//   }
+// });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './build/index.html'));
+// });
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'client/build/index.html'));
@@ -92,19 +104,19 @@ app.get('*', (req, res) => {
 // ======== routes =========
 
 
-const getUsers = (req, res) => {
-    console.log("getUsers")
-    try {
-        pool.query('SELECT * FROM public.users')
-        .then(data => {
-            return res.status(200).json(data.rows[0]);
-        })
-        .catch(err => res.status(204).json({ userFound: "No User Found" + err }));
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-};
+// const getUsers = (req, res) => {
+//     console.log("getUsers")
+//     try {
+//         pool.query('SELECT * FROM public.users')
+//         .then(data => {
+//             return res.status(200).json(data.rows[0]);
+//         })
+//         .catch(err => res.status(204).json({ userFound: "No User Found" + err }));
+//     } catch (err) {
+//         console.error(err);
+//         res.send("Error " + err);
+//     }
+// };
 
 // ======== middlware =========
 // app.use(bodyParser.json());
@@ -121,47 +133,47 @@ const getUsers = (req, res) => {
 //     .route('users')
 //     .get(getUsers);
 // ====== create a user ========
-app.post('/users/registration', async (req, res) => {
-    console.log("users/registration");
-    try {
-        const { id, email, password, role } = req.body;
-        const newTodo = await pool.query(
-          "INSERT INTO users (id, email, password, role) VALUES($1,$2,$3,$4) RETURNING *",
-          [id, email, password, role]
-        );
-        console.log(newTodo.rows[0]);
-        res.json(newTodo.rows[0]);
-      } catch (err) {
-        console.error(err.message);
-      }
-})
+// app.post('/users/registration', async (req, res) => {
+//     console.log("users/registration");
+//     try {
+//         const { id, email, password, role } = req.body;
+//         const newTodo = await pool.query(
+//           "INSERT INTO users (id, email, password, role) VALUES($1,$2,$3,$4) RETURNING *",
+//           [id, email, password, role]
+//         );
+//         console.log(newTodo.rows[0]);
+//         res.json(newTodo.rows[0]);
+//       } catch (err) {
+//         console.error(err.message);
+//       }
+// })
 
 
 // ====== get users ========
-app.get('/users', async (req, res) => {
-    console.log("users")
-    try {
-        const users = await pool.query('SELECT * FROM users');
-        res.json(users.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+// app.get('/users', async (req, res) => {
+//     console.log("users")
+//     try {
+//         const users = await pool.query('SELECT * FROM users');
+//         res.json(users.rows);
+//     } catch (err) {
+//         console.error(err.message);
+//     }
+// })
 
 //get a todo
 
-app.get("/users/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-      const user = await pool.query("SELECT * FROM users WHERE id = $1", [
-        id
-      ]);
+// app.get("/users/:id", async (req, res) => {
+//     try {
+//       const { id } = req.params;
+//       const user = await pool.query("SELECT * FROM users WHERE id = $1", [
+//         id
+//       ]);
   
-      res.json(user.rows[0]);
-    } catch (err) {
-      console.error(err.message);
-    }
-  });
+//       res.json(user.rows[0]);
+//     } catch (err) {
+//       console.error(err.message);
+//     }
+//   });
 
 
 app.listen(port, () => {
