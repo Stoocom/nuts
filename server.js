0f28eaf51bbbdff1a423448f71d5b27686be2540
Alex <test@mail.ru>
2022-07-02 09:19:25 +0300

    fixed some

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 3001;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "build")));
   app.use(favicon(__dirname + "/build/favicon.ico"));
-  console.log(process.env.NODE_ENV === "production");
+  //console.log(process.env.NODE_ENV === "production");
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use("/api", router);
