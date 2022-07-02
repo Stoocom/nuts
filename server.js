bbb4cb08c2d650891ecb4f8a2bffc817eb81a75f
Alex <test@mail.ru>
2022-07-02 20:35:31 +0300

    changed index1

--- server.js
+++ server.js
@@ -19,12 +19,14 @@ const {
 const port = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-  app.use(favicon(__dirname + "/build/favicon.ico"));
-  //console.log(process.env.NODE_ENV === "production");
-  // app.use(express.json());
-  // app.use(express.urlencoded({ extended: true }));
+
+  //app.use(favicon(__dirname + "/build/favicon.ico"));
+  console.log(process.env.NODE_ENV === "production");
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
   app.use("/api", router);
+  app.use(express.static(path.join(__dirname, "build")));
 
   // app.get("/products", async (req, res) => {
   //   console.log("products");
