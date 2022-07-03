6192867c27a3e0c98b868d7a9af6888d0c4b78e1
Alex <test@mail.ru>
2022-07-03 21:46:34 +0300

    fixed server.js

--- server.js
+++ server.js
@@ -17,76 +17,26 @@ const {
 } = require("./models/models");
 
 const port = process.env.PORT || 3001;
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-  app.use(favicon(__dirname + "/build/favicon.ico"));
-  console.log(process.env.NODE_ENV === "production");
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
-  app.use("/api", router);
-  //app.use(express.static(path.join(__dirname, "build")));
-
-  // app.get("/products", async (req, res) => {
-  //   console.log("products");
-  //   try {
-  //     const products = await pool.query("SELECT * FROM products");
-  //     res.json(products.rows);
-  //   } catch (err) {
-  //     console.error(err.message);
-  //   }
-  // });
-
-  // app.get("/types", async (req, res) => {
-  //   console.log("types from Server");
-  //   try {
-  //     const types = await pool.query("SELECT * FROM types");
-  //     res.json(types.rows);
-  //   } catch (err) {
-  //     console.error(err.message);
-  //   }
-  // });
-
-  // app.get("/types/:id", async (req, res) => {
-  //   console.log("types" + req.params.id);
-  //   try {
-  //     const products = await pool.query(
-  //       `SELECT * FROM products WHERE type_id === ${req.params.id}`
-  //     );
-  //     res.json(products.rows);
-  //   } catch (err) {
-  //     console.error(err.message);
-  //   }
-  // });
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/build/index.html"));
-  });
-}
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api", router);
-// app.get('/', (req, res) => {
-//     console.log("api");
-//     console.log(process.env.NODE_ENV);
-//     //res.send({ message: "Hello world"});
-//     res.status(200).json({ message: "I am here!"});
-//   });
-
-// app.get('/users', async (req, res) => {
-//   console.log("users")
-//   try {
-//       const users = await pool.query('SELECT * FROM users');
-//       res.json(users.rows);
-//   } catch (err) {
-//       console.error(err.message);
-//   }
-// });
+if (process.env.NODE_ENV === "production") {
+  // production
+  app.use(express.static(path.join(__dirname, "build")));
+  app.use(favicon(__dirname + "/build/favicon.ico"));
+  app.use("/api", router);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/build/index.html"));
+  });
+} else {
+  // development
+  app.use(express.static(path.join(__dirname, "public")));
+  app.use("/api", router);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+}
 
 app.use((err, req, res, next) => {
   console.log("После миддвара");
@@ -94,19 +44,11 @@ app.use((err, req, res, next) => {
     console.log(err);
     return res.status(err.status).json({ message: err.message });
   }
-  //console.log(err);
-  //res.status(500);ss
-  //res.status(500).json({error: 'an error occurred'});
   return res.status(500).json({ message: "Непредвиденная ошибка!" });
-  //return res.status(500).send({ error: 'Something failed!' });
 });
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, './build/index.html'));
-// });
 
 async function start() {
   try {
